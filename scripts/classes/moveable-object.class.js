cf2001d048ff84f4c5a2ebba40e865df0025d84a
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-06-04 16:50:54 +0200

    added frame to character and chicken entity

--- scripts/classes/moveable-object.class.js
+++ scripts/classes/moveable-object.class.js
@@ -52,11 +52,13 @@ class MoveableObject {
   }
 
   drawFrame(ctx) {
-    ctx.beginPath();
-    ctx.lineWidth = "5";
-    ctx.strokeStyle = "blue";
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "5";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
   }
 
   positionEntity(img, factorHeight, factorWidth) {
