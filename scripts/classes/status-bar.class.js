37025ea26fb336f69b41d08e37f4aeaa22cd055d
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-06-10 15:11:54 +0200

    updated todo

--- scripts/classes/status-bar.class.js
+++ scripts/classes/status-bar.class.js
@@ -1,14 +1,21 @@
 class StatusBar extends DrawableObject {
   world;
-  IMAGE_HEALTH_CHARACTER = ["img/7_statusbars/3_icons/icon_health.png"];
+  IMAGE_HEALTH_CHARACTER = "img/7_statusbars/3_icons/icon_health.png";
 
   constructor(world) {
     super();
     this.world = world;
-    this.loadimages(this.IMAGE_HEALTH_CHARACTER);
+    this.loadImage(this.IMAGE_HEALTH_CHARACTER);
     this.x = 0;
     this.y = 0;
-    this.width = 200;
-    this.height = 50;
+    this.width = 100;
+    this.height = 100;
+    this.drawCharacterHealth();
+  }
+
+  drawCharacterHealth() {
+    this.world.ctx.font = "20px Arial";
+    this.world.ctx.fillStyle = "white";
+    this.world.ctx.fillText("Test", 50, 50);
   }
 }
--- todo.md
+++ todo.md
@@ -1,1 +1,5 @@
 # To-Do Liste für El-Pollo-Locco
+
+- fix character health in statusbar class
+- fix collision between character and enemy
+- delete dead enemy from level enemies array
